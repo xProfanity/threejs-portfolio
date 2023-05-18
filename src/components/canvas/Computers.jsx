cc9c53b36a477c68a3c2d86ece2196c59d5ca0c5
Got-crypto <pecpalmplay@gmail.com>
2023-05-18 12:17:21 +0200

    creating about section; about to start HOC

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -3,7 +3,6 @@ import { Canvas } from "@react-three/fiber"
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei"
 
 import CanvasLoader from '../Loader'
-import { PointLight } from "three.js"
 
 const Computers = ({isMobile}) => {
   const computer = useGLTF('./desktop_pc/scene.gltf')
