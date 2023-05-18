d03d0827b85524a82ea2575c30dea07a3f12c02b
Got-crypto <pecpalmplay@gmail.com>
2023-05-19 01:05:59 +0200

    finished contact section; three.js is fun!

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -26,7 +26,7 @@ const Navbar = () => {
             } hover:text-white text-[18px] font-medium cursor-pointer`}
               onClick={()=> setActive(link.title)}
             >
-              <a href={`${link.id}`}>{link.title}</a>
+              <a href={`#${link.id}`}>{link.title}</a>
             </li>
           ) )}
         </ul>
